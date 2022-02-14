aa5c7e55083b662ff4a5a9e466d09449372db5f7
ZARIFMAC <uzbzarif@gmail.com>
2022-02-14 16:34:50 +0900

    polygon

--- polygon.py
+++ polygon.py
@@ -22,3 +22,68 @@ class Polygon:
         vertices: a list of vertices each element of the list is a pair of coordinates (x, y)
         """
         self.vertices = vertices
+
+    #Define the method to validate the polygon
+    def is_valid(self) -> bool:
+        """
+        Validate the polygon.
+        Returns:
+            True if the polygon is valid
+            False if the polygon is invalid
+        """
+        pass
+    
+    # Calculate the area of the polygon
+    def area(self) -> float:
+        """
+        Calculate the area of the polygon.
+        """
+        pass
+    # Distance between two points
+    def distance(self, p1: tuple, p2: tuple) -> float:
+        """
+        Calculate the distance between two points.
+        Args:
+            p1: a pair of coordinates (x, y)
+            p2: a pair of coordinates (x, y)
+        Returns:
+            the distance between p1 and p2
+        """
+        pass
+
+    #Define the method to get all sides of the length of the polygon
+    def sides(self) -> list:
+        """
+        Get all sides of the polygon.
+        Returns:
+            a list of all sides of the polygon
+        """
+        pass
+     
+     # Define the method to calculate the perimeter of the polygon
+    def perimeter(self) -> float:
+        """
+        Calculate the perimeter of the polygon.
+        Returns:
+            the perimeter of the polygon
+        """
+        pass
+
+    #Define the method to calculate the angle between two sides
+    def angles(self) -> list:
+        """
+        Calculate the angles of the polygon.
+        Returns:
+            a list of all angles of the polygon
+        """
+        pass
+        
+    #Define the method to calculate the centroid of the polygon
+    def centroid(self) -> tuple:
+        """
+        Calculate the centroid of the polygon.
+        Returns:
+            a pair of coordinates (x, y)
+        """
+        pass
+
